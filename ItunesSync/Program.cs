50dbb61886707c1defca03117c6f81b679070821
sgulliver <scott.gulliver@gmail.com>
2013-10-27 14:08:38 +0000

    Improved the error handling, and improved speed.

--- ItunesSync/Program.cs
+++ ItunesSync/Program.cs
@@ -56,6 +56,12 @@ namespace ItunesSync
 
             Console.WriteLine("Share -> Local..");
 
+            var localFiles = new List<String>();
+            foreach(IITFileOrCDTrack file in playlist.Tracks)
+            {
+                localFiles.Add(file.Location);
+            }
+
             var remoteFiles = Directory.GetFiles(remotePath);
             foreach (var file in remoteFiles)
             {
@@ -63,11 +69,12 @@ namespace ItunesSync
                     false;
 
                 //ensure that the file hasn't already been added
-                foreach (IITFileOrCDTrack playlistTrack in playlist.Tracks)
+                foreach (String playlistTrack in localFiles)
                 {
-                    if (Path.GetFileName(playlistTrack.Location) == Path.GetFileName(file))
+                    if (Path.GetFileName(playlistTrack) == Path.GetFileName(file))
                     {
                         trackAlreadyExists = true;
+                        localFiles.Remove(playlistTrack);
                         break;
                     }
                 }
@@ -89,20 +96,35 @@ namespace ItunesSync
 
             Console.WriteLine("Local -> Share..");
 
+            var errors = new List<String>();
+
             foreach (IITFileOrCDTrack playlistTrack in playlist.Tracks)
             {
                 var targetPath = Path.Combine(remotePath, Path.GetFileName(playlistTrack.Location));
                 if (!File.Exists(targetPath))
                 {
-                    File.Copy(playlistTrack.Location, targetPath);
-                    Console.WriteLine("Copied " + Path.GetFileName(playlistTrack.Location));
+                    try
+                    {
+                        File.Copy(playlistTrack.Location, targetPath);
+                        Console.WriteLine("Copied " + Path.GetFileName(playlistTrack.Location));
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("Error copying " + Path.GetFileName(playlistTrack.Location) + " (" + ex.Message + ")");
+                        errors.Add(Path.GetFileName(playlistTrack.Location));
+                    }
                 }
             }
 
-            Console.WriteLine("..Done!");
+            Console.WriteLine(errors.Any() ? "..Done, with the following errors:" : "..Done!");
+            foreach (var error in errors)
+            {
+                Console.WriteLine(error);
+            }
             Console.WriteLine();
 
-            Thread.Sleep(5000);
+            Console.WriteLine("Press enter to quit..");
+            Console.ReadLine();
         }
     }
 }
